3ceb4cf72d2bcf126a467fb9ea6044b1b0bb3b9a
Daniel Chong <danielgq.chong@gmail.com>
2016-10-20 09:32:28 +1300

    move view code to mainscreen.js

--- App/Root.js
+++ App/Root.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View
-} from 'react-native';
 
-import styles from './Container/Styles/MainScreen.Style.js'
+import MainScreen from './Containers/MainScreen.js'
 
-export default class WebQem_RN extends Component {
+export default class Root extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+      <MainScreen />
+    )
   }
 }
