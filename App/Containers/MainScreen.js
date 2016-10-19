b7380785c22b67841a2ac391c2f0917d0d50bdd8
Daniel Chong <danielgq.chong@gmail.com>
2016-10-20 10:06:23 +1300

    make sure the forms are working

--- App/Containers/MainScreen.js
+++ App/Containers/MainScreen.js
@@ -8,8 +8,31 @@ import { Button, FormLabel, FormInput} from 'react-native-elements'
 import styles from './Styles/MainScreen.Style.js'
 
 export default class MainScreen extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      type:'uint8',
+      numOfRandomNum: 10
+    }
+    this.changeNumberType = this.changeNumberType.bind(this)
+    this.getMoreRandomNumber = this.getMoreRandomNumber.bind(this)
+    this.changeNumberOfRandomNumber = this.changeNumberOfRandomNumber.bind(this)
+  }
+
   getMoreRandomNumber() {
 
+  }
+
+  changeNumberType() {
+    if (this.state.type == 'uint8') {
+      this.setState({ type: 'uint16'})
+    } else {
+      this.setState({ type: 'uint8'})
+    }
+  }
+
+  changeNumberOfRandomNumber(value) {
+    this.setState({numOfRandomNum: value})
   }
 
   render() {
@@ -19,13 +42,15 @@ export default class MainScreen extends Component {
           <View style={styles.textRow}>
             <FormInput
               placeholder='Num of Random Numbers'
+              keyboardType='numeric'
+              value={this.state.numOfRandomNum.toString()}
               onChangeText={this.changeNumberOfRandomNumber}/>
           </View>
           <View style={styles.fullRow}>
             <Button
               small
               onPress={this.changeNumberType}
-              title='uint8' />
+              title={this.state.type} />
           </View>
         </View>
         <Button
