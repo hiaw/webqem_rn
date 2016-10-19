c6e36a423185c825d5ad203dc37d1d7d1b6f0781
Daniel Chong <danielgq.chong@gmail.com>
2016-10-20 09:51:00 +1300

    basic input

--- App/Containers/MainScreen.js
+++ App/Containers/MainScreen.js
@@ -3,23 +3,35 @@ import {
   Text,
   View
 } from 'react-native';
+import { Button, FormLabel, FormInput} from 'react-native-elements'
 
 import styles from './Styles/MainScreen.Style.js'
 
 export default class MainScreen extends Component {
+  getMoreRandomNumber() {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.row}>
+          <View style={styles.textRow}>
+            <FormInput
+              placeholder='Num of Random Numbers'
+              onChangeText={this.changeNumberOfRandomNumber}/>
+          </View>
+          <View style={styles.fullRow}>
+            <Button
+              small
+              onPress={this.changeNumberType}
+              title='uint8' />
+          </View>
+        </View>
+        <Button
+          small
+          onPress={this.getMoreRandomNumber}
+          title='Request' />
       </View>
     );
   }
